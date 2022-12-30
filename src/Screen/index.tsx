2fc6854bcceabcbb49911e4cf3054094a73b1591
Apple <apple@Hitendras-MacBook-Pro.local>
2022-12-30 13:57:59 +0530

    multiply table

--- src/Screen/index.tsx
+++ src/Screen/index.tsx
@@ -3,43 +3,44 @@ import Button from '../components/Button';
 import InputBox from '../components/InputBox';
 
 
-
 const MainScreen = () => {
-    const [userName, setUserName] = useState('');
-    const [password, setPasswordText] = useState('');
+    const [color, setColorState] = useState('Hiten');
+    const [table, setNumber] = useState<any>([]);
+    let colorName: string = ''
+    let inputNumber: number = 0;
 
-    // First and then Always when state changes(means when state re-renders)
-    useEffect(() => {
-        console.log('parent useEffect componentWillMount')
-    })
-
-    //Only once when component mounts , means only first time
-    useEffect(() => {
-        console.log('parent useEffect componentDidMount')
-    },[])
-
-    // call when passing state/props values changes
-    useEffect(()=> {
-        if (userName.includes('@')) {
-            alert('valid')
-        }
-    },[userName]) //will trigger based on changes of these passing variables  
-
-    const setUserText = (value: string) => {
-        if (value.includes('@')) {
-            alert('valid')
-        }
-        //setUserName(value)
+    const setColor = (value:number) => {
+       // colorName = value;
+       inputNumber = value
     }
 
-    const setPassword = (password: string) => {
-        setPasswordText(password)
+    const onButtonClick = () => {
+      for(let i= 1 ; i<=10 ; i++) {
+        const value = inputNumber * i;
+        table.push(value)
+        setNumber([...table])
+      }
     }
 
+   
+  const drawTable = () => {
+      const tableElement = table.map((item: number, index: number) => {
+          return(
+              <h1 key={index}>{item}</h1>
+          )
+      })
+      console.log('tableElement',tableElement)
+      return tableElement;
+  }
+
+  console.log('table syae', table)
+    
     return (
-        <div>
-          <InputBox setInputText={setUserText} inpuText={userName} />
-          <InputBox setInputText={setPassword} inpuText={userName} />
+        <div style={{backgroundColor: color}}>
+          {/* <InputBox setInputText={setUserText} inpuText={''} /> */}
+          <button className='button' onClick={onButtonClick}>{'Click me'}</button>
+          <InputBox setInputText={setColor} />
+          {drawTable()}
         </div>
     )
 }
---
+++ src/helper/getData.ts
@@ -0,0 +1,4 @@
+export const getMultiply = (num1: number, num2: number) => {
+    const mul = num1 * num2;
+    return mul;
+}
